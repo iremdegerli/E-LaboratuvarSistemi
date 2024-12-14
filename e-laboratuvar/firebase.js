4d36f84aa20c15017cf2813d75da53d8072f752d
iremdegerli <degerliirem1@gmail.com>
2024-12-14 18:29:47 +0300

    Firebase

--- e-laboratuvar/firebase.js
+++ e-laboratuvar/firebase.js
@@ -1,21 +1,36 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import React, { useState, useEffect } from 'react';
+import { View, Text } from 'react-native';
+import auth from '@react-native-firebase/auth';
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyCNp6pob7qLHgBf-Jc-tONLILxdR-zGhwQ",
-  authDomain: "e-labo-proje.firebaseapp.com",
-  projectId: "e-labo-proje",
-  storageBucket: "e-labo-proje.firebasestorage.app",
-  messagingSenderId: "1073158315840",
-  appId: "1:1073158315840:web:70a65cba8f1c9634b824a5",
-  measurementId: "G-8QYS5Z9XFS"
-};
+function App() {
+  // Set an initializing state whilst Firebase connects
+  const [initializing, setInitializing] = useState(true);
+  const [user, setUser] = useState();
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+  // Handle user state changes
+  function onAuthStateChanged(user) {
+    setUser(user);
+    if (initializing) setInitializing(false);
+  }
+
+  useEffect(() => {
+    const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+    return subscriber; // unsubscribe on unmount
+  }, []);
+
+  if (initializing) return null;
+
+  if (!user) {
+    return (
+      <View>
+        <Text>Login</Text>
+      </View>
+    );
+  }
+
+  return (
+    <View>
+      <Text>Welcome {user.email}</Text>
+    </View>
+  );
+}

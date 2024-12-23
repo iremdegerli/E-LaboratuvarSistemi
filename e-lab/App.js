c1ffe2bdc800949e81b90a03f272c21d97addb42
bbeyzacagli <beyzacagli2@gmail.com>
2024-12-23 23:31:40 +0300

    Update App.js

--- e-lab/App.js
+++ e-lab/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { Text, View, useState, useEffect } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { firebase } from "./firebaseConfig";
@@ -8,7 +8,25 @@ import HomeScreen from "./components/HomeScreen";
 //import ProfileScreen from "./components/ProfileScreen";
 //import EditProfileScreen from "./components/EditProfileScreen";
 
+<<<<<<< Updated upstream
 const Tab = createBottomTabNavigator();
+=======
+
+
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <View>
+      <Text>E-Laboratuvar Sistemi</Text>
+      <Register /> {/* Kayıt bileşenini çağır */}
+    </View>
+  );
+}
+export default App;
+/*
+export default App;
+>>>>>>> Stashed changes
 
 export default function App() {
   return (
